4be2eaadfae892d43d84738d41d3ee4d7016e65c
freefiresingapore24535-alt <freefiresingapore24535@gmail.com>
2025-12-09 18:10:40 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -16,27 +16,30 @@ from database.database import *
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-
     if data == "help":
         await query.message.edit_text(
             text=HELP_TXT.format(first=query.from_user.first_name),
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("ʜᴏᴍᴇ", callback_data='/start'),
-                 InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data='close')]
-            ])
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
+                        InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data='close')
+                    ]
+                ]
+            )
         )
-
     elif data == "about":
         await query.message.edit_text(
             text=ABOUT_TXT.format(first=query.from_user.first_name),
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
-                 InlineKeyboardButton('ᴄʟᴏꜱᴇ', callback_data='close')]
-            ])
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
+                     InlineKeyboardButton('ᴄʟᴏꜱᴇ', callback_data='close')]
+                ]
+            )
         )
-
     elif data == "start":
         await query.message.edit_text(
             text=START_MSG.format(first=query.from_user.first_name),
